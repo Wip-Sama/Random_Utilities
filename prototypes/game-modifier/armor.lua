cb1357fd06ad499df9156ddbce12a535dfdec8ca
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2020-08-02 03:31:56 +0200

    1.3.3

--- prototypes/game-modifier/armor.lua
+++ prototypes/game-modifier/armor.lua
@@ -3,29 +3,29 @@ data:extend(
   {
     type = "equipment-grid",
     name = "RU-01-equipment-grid",
-    width = 12,
-    height = 12,
+    width = 8,
+    height = 8,
     equipment_categories = {"armor"}
   },
   {
     type = "equipment-grid",
     name = "RU-02-equipment-grid",
-    width = 14,
-    height = 14,
+    width = 10,
+    height = 10,
     equipment_categories = {"armor"}
   },
   {
     type = "equipment-grid",
     name = "RU-03-equipment-grid",
-    width = 16,
-   height = 16,
+    width = 12,
+    height = 14,
     equipment_categories = {"armor"}
   },
   {
     type = "equipment-grid",
     name = "RU-04-equipment-grid",
-    width = 18,
-   height = 18,
+    width = 16,
+   height = 16,
     equipment_categories = {"armor"}
   },
 }
